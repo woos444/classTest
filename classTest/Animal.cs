6af33a3d18e4dd07dccef3516e2fbf4af08ab3f1
wos <rladntjq444@naver.com>
2019-05-31 15:12:25 +0900

    초기 커밋.

---
+++ classTest/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classTest
+{
+    class Animal
+    {
+        private string name;
+        private string sound;
+
+        public Animal(){}
+
+        public Animal(string n,string a)
+        {
+            name = n;
+            sound = a;
+        }
+        public void MakeSound()
+        {
+            Console.WriteLine("Animal name : {0}, sound : {1}",name,sound);
+        }
+    }
+}
---
+++ classTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Rectangle rect1;
+             rect1.leng = 100;
+             rect1.wid = 30;
+
+             Console.WriteLine("rect leng : {0}, wid :{1}", rect1.leng, rect1.wid);
+
+             Rectangle rect2 = new Rectangle(200,50);
+             Console.WriteLine("rect leng : {0}, wid :{1}, Area :{2} ", rect2.leng, rect2.wid, rect2.Area());
+             */
+
+
+            Animal fox = new Animal("fox","Raaww");
+            fox.MakeSound();
+            Animal a = new Animal("Wolf","Walllll");
+            a.MakeSound();
+            Animal b = new Animal("bar","GomGom");
+            b.MakeSound();
+
+            Console.ReadLine();
+
+
+
+
+        }
+
+        struct Rectangle
+        {
+            public double leng;
+            public double wid;
+
+            public Rectangle(double l,double w)
+            {
+                leng = l;
+                wid = w;
+            }
+
+            public double Area()
+            {
+                return leng * wid;
+            }
+
+        }
+    }
+}
